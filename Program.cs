a506dcc59aedebaa8ee0c6971444fbb8fa54ed52
Gabriel-Gabiru-Windows <gabriel_librande@hotmail.com>
2022-12-19 01:50:20 -0300

    CommitDioIntroducao.NET

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DIO_intro.NET.Models;
+
+Pessoa varPerson = new Pessoa();
+
+varPerson.Name = "Pedro";
+varPerson.Age = 22;
+
+varPerson.Apresentar();
+
+varPerson.HorarioeDia();
---
+++ README.md
@@ -0,0 +1,3 @@
+# DIO-Introdução ao .NET  
+
+## atividade feita durante a aula de Introdução ao .NET no Bootcamp da DIO.
